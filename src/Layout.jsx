f7499ddb5551077d0a1ed93ef531aaa1215de8ca
Wishmastering <charsdb@gmail.com>
2023-02-07 18:08:30 -0600

    first push

---
+++ src/Layout.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { BrowserRouter } from "react-router-dom";
+
+
+function Layout() {
+  return (
+    <div className="App">
+      <h1> Hey!</h1>
+    </div>
+  );
+}
+
+export default Layout;
