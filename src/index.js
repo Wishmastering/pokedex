c2232c608d8f39cf110b96add8240faccc81fbd9
Wishmastering <charsdb@gmail.com>
2023-02-18 16:04:24 -0600

    Galeria funcionando

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Layout from './Layout';
 import PokemonContext from "./page/Context"
+import axios from "axios";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,14 +19,23 @@ root.render(
 function ContextWrapper({children}){
   
   let [store,setStore] = useState({
-    pokemon: "Ha ",
+    pokemon: "",
     pokemonList: [],
     counter: 0,
   })
   let actions = {
-    getPokemon: ()=>{
-      console.log(store.pokemon)
-    },
+    getPokemon:()=>{
+      axios.get('https://pokeapi.co/api/v2/pokemon')
+    .then(function (response) {
+      // handle success
+      setStore({...store, pokemonList: response.data.results});
+      console.log(store.pokemonList)
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+  },
     getPokemonList: ()=>{
       console.log(store.pokemonList)
     },
@@ -35,7 +45,7 @@ function ContextWrapper({children}){
                   store.counter === 0 ? setStore({...store, counter: store.counter-1})
                                   :
                                   setStore({...store, counter: store.counter-1})
-    }
+    },
   }
 
   return<>
--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -1,6 +1,5 @@
 import '../styles.css';
 import React, {useState, useEffect, useContext} from "react";
-import axios from "axios";
 import PokemonInfo from '../components/PokemonInfo';
 import PokemonContext from './Context';
 
@@ -10,38 +9,27 @@ export default function Landing(){
 
 const [store,actions] = useContext(PokemonContext) // Consumir contexto
 
- let [info, setInfo] = useState([])
+ 
  let [show, setShow] = useState(false)
-
-  function getPokemon(){
-    axios.get('https://pokeapi.co/api/v2/pokemon')
-  .then(function (response) {
-    // handle success
-    setInfo(response.data.results);
-    console.log(info)
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-}
 
 function showPokemon(){
     show ? setShow(false) : setShow(true)
 }
 
 useEffect(()=>{
-    getPokemon() 
+    actions.getPokemon() 
 },[])
 
 
     return<>
         <div className="App">
+          <button onClick={()=> console.log(store?.pokemonList[store.counter]?.name)}>
+             Consoole log</button>
             <h1> Landing Page!</h1>
             <button onClick={()=> showPokemon()}> Check The Available Pokemons!</button>
               { show ? 
                   <div className='d-flex'>
-                      <PokemonInfo value={info[store.counter].name} />
+                      <PokemonInfo value={store?.pokemonList[store.counter]?.name} />
                   </div>: ""
               }
         </div>
